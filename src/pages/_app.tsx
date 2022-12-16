9c6fac67fb12e22f9262baac2c6acc6a4cab04e7
Thiago Lins <falecom@thiagolins.dev.br>
2022-12-15 22:25:46 -0300

    [Add] HomePage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '@/styles/global'
 import { dark } from '@/styles/themes/dark'
 import { light } from '@/styles/themes/light'
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -15,8 +14,9 @@ export default function App({ Component, pageProps }: AppProps) {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="icon" type="image/x-icon" href="/img/icon_512.png" />
+          <link rel="shortcut" href="/img/icon_512.png" />
+          <link rel="apple-touch-icon" href="/img/icon_512.png" />
           <meta
             name="description"
             content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
