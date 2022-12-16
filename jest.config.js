e84bf1ccf5e9a04d26103964ec58ffd113e7e121
Thiago Lins <falecom@thiagolins.dev.br>
2022-12-15 21:56:18 -0300

    [Start] Project With NextJS [Start] Project With Eslint + Prettier [Start] Project With Husk + Lint Staged [Start] Project With Jest [Start] Project With Styled-Components

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// jest.config.js
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './'
+})
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom'
+}
+
+module.exports = createJestConfig(customJestConfig)
---
+++ src/pages/_app.tsx
@@ -0,0 +1,33 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from '@/styles/global'
+import { dark } from '@/styles/themes/dark'
+import { light } from '@/styles/themes/light'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <meta
+            name="description"
+            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          />
+          <title>Boilerplate - NextJs/Typescript</title>
+        </Head>
+      </Head>
+      <ThemeProvider theme={light || dark}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
